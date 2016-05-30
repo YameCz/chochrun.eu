d809c609262c9bb0abe41721800ec34b2875b7ba
Dominik Nedvědík <yamecz1@gmail.com>
2016-05-30 22:22:20 +0200

    contact table

--- kontakt.php
+++ kontakt.php
@@ -14,10 +14,55 @@
                     <div id="body">
                     	<h1>Kontaktujte nás</h1>
                     	<p>Nebojíme se zodpovědět všechny vaše dotazy, ať už se jedná o technické či herní otázky, odpovíme vám na cokoliv, co se našich služeb týče.<br>Kontaktovat nás můžete mnoha způsoby.</p>  
-						<h2>Mr. Chochrun - majitel serveru</h2>
-                    	<p>Hlavní mástr. Stará se o běh a financování serveru a je nenahraditelným článkem týmu, bez kterého by se žádná sranda nekonala.<br>E-mail: <a href="mailto:">nejaky.email@email.com</a><br>TeamSpeak: Mr. Chochrun</a></p>  
-						<h2>YameCz - správce</h2>
-						<p>Email: <a href="mailto:info@nedvedik.eu">info@nedvedik.eu</a><br>TeamSpeak: YameCz</p>
+						<h2>Vedení</h2>
+                    	<table>
+                            <tr>
+                                <th>Jméno</th>
+                                <th>Funkce</th>
+                                <th>E-mail</th>
+                            </tr>
+                            <tr>
+                                <td>Mr. Chochrun</td>
+                                <td>Správce portálu</td>
+                                <td><a href="mailto:chochrunl@post.cz">chochrunl@post.cz</a></td>
+                            </tr>
+                            <tr>
+                                <td>YameCz</td>
+                                <td>Svatý Veterán</td>
+                                <td><a href="mailto:dominik@nedvedik.eu">dominik@nedvedik.eu</a></td>
+                            </tr>
+                        </table>
+                        <h2>TeamSpeak 3 Server</h2>
+                        <table>
+                            <tr>
+                                <th>Jméno</th>
+                                <th>Funkce</th>
+                                <th>E-mail</th>
+                            </tr>
+                            <tr>
+                                <td>Craszh</td>
+                                <td>Administrátor</td>
+                                <td><a href="mailto:letnyvu@seznam.cz">letnyvu@seznam.cz</a></td>
+                            </tr>
+                            <tr>
+                                <td>Reini</td>
+                                <td>Administrátor</td>
+                                <td><a href="mailto:reiniMB@gmail.com">reiniMB@gmail.com</a></td>
+                            </tr>
+                        </table>
+                        <h2>Akvário</h2>
+                        <table>
+                            <tr>
+                                <th>Jméno</th>
+                                <th>Funkce</th>
+                                <th>E-mail</th>
+                            </tr>
+                            <tr>
+                                <td>mr. nobody</td>
+                                <td>Administrátor</td>
+                                <td><a href="kajista8@gmail.com">kajista8@gmail.com</a></td>
+                            </tr>
+                        </table>
 					</div>
                     <!-- Body end -->
                     
